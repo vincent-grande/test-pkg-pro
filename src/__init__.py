7a07ad8cae372d59b183c9b8f4897dbd1a025413
Vincent Grande <grande@cs.rwth-aachen.de>
2024-01-09 15:45:34 +0100

    ...

---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from .algorithm import useful_function
---
+++ src/algorithm.py
@@ -0,0 +1,3 @@
+def useful_function(number,factor, string):
+    """This function does something useful"""
+    return number * factor, string
